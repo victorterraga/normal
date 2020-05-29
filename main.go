c986d8be262a7dfb529ae27fd6c901ffe7082348
Víctor Terraga <victorterraga@gmail.com>
2020-05-29 16:35:57 +0200

    dataset practica 2

--- main.go
+++ main.go
@@ -4,24 +4,29 @@ import (
 	"flag"
 	"fmt"
 	"math/rand"
-	"os"
 )
 
-func getNormDistro(x int, mu float64, sigma float64) {
-	for i := 0; i < x; i++ {
-		fmt.Printf("%.2f\n", rand.NormFloat64()*float64(sigma)+float64(mu))
+func getNormDistro(mu float64, sigma float64) {
+	fmt.Println("f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,answer")
+	for i := 0; i < 2000; i++ {
+		f1 := rand.NormFloat64()*float64(sigma) + float64(mu)
+		f2 := rand.NormFloat64()*float64(sigma) + float64(mu)
+		f3 := rand.NormFloat64()*float64(sigma) + float64(mu)
+		f4 := rand.NormFloat64()*float64(sigma) + float64(mu)
+		f5 := rand.NormFloat64()*float64(sigma) + float64(mu)
+		f6 := f1 + f2 - f4
+		f7 := f1 + 3*f3
+		f8 := f3 * f4
+		f9 := f4 * 2 * f5
+		f10 := f1 + f2 + f3 + f4
+		answer := f1 + f2 + 2*f5 + f8
+		fmt.Printf("%.6f,%.6f,%.6f,%.6f,%.6f,%.6f,%.6f,%.6f,%.6f,%.6f,%.6f\n", f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, answer)
 	}
 }
 
 func main() {
-	n := flag.Int("n", 0, "Number of numbers")
-	m := flag.Float64("m", 0, "Mean")
-	s := flag.Float64("s", 0, "Standard distribution")
+	m := flag.Float64("m", 1, "Mean")
+	s := flag.Float64("s", 0.75, "Standard distribution")
 	flag.Parse()
-
-	if *n == 0 {
-		flag.Usage()
-		os.Exit(1)
-	}
-	getNormDistro(*n, *m, *s)
+	getNormDistro(*m, *s)
 }

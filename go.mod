31e6f03b6ccc6d87284a7b35bac669814f40cc22
Víctor Terraga <victorterraga@gmail.com>
2020-05-29 19:15:15 +0200

    interim

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/victorterraga/normal
 
 go 1.14
+
+require gonum.org/v1/gonum v0.7.0
--- main.go
+++ main.go
@@ -3,23 +3,37 @@ package main
 import (
 	"flag"
 	"fmt"
-	"math/rand"
+	"gonum.org/v1/gonum/stat/distuv"
 )
 
 func getNormDistro(mu float64, sigma float64) {
 	fmt.Println("f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,answer")
+	e := distuv.Exponential{
+		Rate: mu,
+	}
+	e2 := distuv.Exponential{
+		Rate: mu * 0.5,
+	}
+	n := distuv.Normal{
+		Mu:    mu,
+		Sigma: sigma,
+	}
+	n2 := distuv.Normal{
+		Mu:    mu * 0.5,
+		Sigma: sigma + 3,
+	}
 	for i := 0; i < 2000; i++ {
-		f1 := rand.NormFloat64()*float64(sigma) + float64(mu)
-		f2 := rand.NormFloat64()*float64(sigma) + float64(mu)
-		f3 := rand.NormFloat64()*float64(sigma) + float64(mu)
-		f4 := rand.NormFloat64()*float64(sigma) + float64(mu)
-		f5 := rand.NormFloat64()*float64(sigma) + float64(mu)
+		f1 := n.Rand()
+		f2 := e.Rand()
+		f3 := e2.Rand()
+		f4 := n.Rand()
+		f5 := n2.Rand()
 		f6 := f1 + f2 - f4
-		f7 := f1 + 3*f3
-		f8 := f3 * f4
-		f9 := f4 * 2 * f5
-		f10 := f1 + f2 + f3 + f4
-		answer := f1 + f2 + 2*f5 + f8 + rand.NormFloat64()*float64(sigma) + float64(mu)
+		f7 := f1 + f3
+		f8 := f3 + f4
+		f9 := f4 * f5
+		f10 := f1 + f2 + f3
+		answer := f1 + f2 + f8 + n2.Rand()
 		fmt.Printf("%.6f,%.6f,%.6f,%.6f,%.6f,%.6f,%.6f,%.6f,%.6f,%.6f,%.6f\n", f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, answer)
 	}
 }
